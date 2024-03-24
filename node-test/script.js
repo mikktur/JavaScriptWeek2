5344ed3d981bb9f1efba0fc627776f36ab99b3cc
mikktur <mikko.silenceri@gmail.com>
2024-03-24 16:59:50 +0200

    all tasks done

---
+++ node-test/script.js
@@ -0,0 +1,10 @@
+console.log('This is JSrcipt');
+console.log('asad');
+
+var lodash = require('lodash');
+var notCamelCase = 'Foo Bar';
+console.log(notCamelCase);
+const camelCase = lodash.camelCase(notCamelCase);
+console.log(notCamelCase);
+console.log(camelCase);
+
